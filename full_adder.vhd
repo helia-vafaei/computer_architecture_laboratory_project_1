bfd1a3a9d666abb9cc0824ac14d5d176a5ce9024
Helia Vafaei <74323500+helia-vafaei@users.noreply.github.com>
2023-01-31 01:49:44 +0330

    Add files via upload

---
+++ full_adder.vhd
@@ -0,0 +1,52 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:28:07 10/25/2022 
+-- Design Name: 
+-- Module Name:    full_adder - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+ library ieee; 
+ use ieee.std_logic_1164.all;  
+ use IEEE.STD_LOGIC_unsigned.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity full_adder is
+    Port ( af : in  STD_LOGIC;
+           bf : in  STD_LOGIC;
+           cin : in  STD_LOGIC;
+           s : out  STD_LOGIC;
+           cout : out  STD_LOGIC);
+end full_adder;
+
+architecture Behavioral of full_adder is
+
+ signal z: STD_LOGIC_vector(1 downto 0);
+ begin  
+   process(af,bf,cin)
+   begin 
+ z <= ('0'& af) + ('0'& bf) +('0'& cin) ;
+   end process;
+   s <= z(0);
+   cout <= z(1);
+ end Behavioral; 
---
+++ half_adder.vhd
@@ -0,0 +1,55 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    21:51:20 10/25/2022 
+-- Design Name: 
+-- Module Name:    half_adder - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity half_adder is
+    Port ( ah : in  STD_LOGIC;
+           bh : in  STD_LOGIC;
+           s : out  STD_LOGIC;
+			  cout : out  STD_LOGIC);
+end half_adder;
+
+architecture Behavioral of half_adder is
+component xor_gate 
+port (
+a,b :in STD_LOGIC;
+c : out STD_LOGIC);
+end component;
+component andgate
+port (
+a,b :in STD_LOGIC;
+c : out STD_LOGIC);
+end component;
+signal z : STD_LOGIC;
+begin 
+xor_ins : xor_gate port map ( a => ah , b => bh ,c=> s);
+and_ins : andgate port map ( a => ah , b => bh ,c => cout);
+
+end Behavioral;
